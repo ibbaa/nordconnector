5929b25df22728ef5c4369fbab82a36c1b24f2fa
alwin <alwin@ibba.de>
2021-02-28 12:15:31 +0100

    conan integration

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 rm -rf build
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" ..
-cmake --build .
+mkdir -p build/Debug
+mkdir -p build/conan
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan install -if build/conan .
+cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B build/Debug .
+cmake --build build/Debug
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+#include "Poco/Timestamp.h"
+#include "Poco/Timezone.h"
+#include "Poco/DateTimeFormatter.h"
+#include "Poco/DateTimeFormat.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello World!\n";
+    Poco::Timestamp now;
+    Poco::DateTimeFormatter formatter;
+    std::string nowstr = formatter.format(now, Poco::DateTimeFormat::SORTABLE_FORMAT, Poco::Timezone::tzd());
+    std::cout << "Hello World! on " << nowstr << "\n";
     return 0;
 }
