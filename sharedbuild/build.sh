e906bb3a9c12b9fb9eed869cd0526f2a0af4930f
alwin <alwin@ibba.de>
2021-03-01 19:29:03 +0100

    dynamic build config

---
+++ sharedbuild/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm -rf build
+mkdir -p build/Debug
+mkdir -p build/conan
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan install -if build/conan .
+cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B build/Debug .
+cmake --build build/Debug
+cp -r build/conan/lib build/Debug
