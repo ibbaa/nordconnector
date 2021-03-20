71e24b5d427cc23cff12432abcfdf4dff0fe037d
alwin <alwin@ibba.de>
2021-03-20 15:02:37 +0100

    sharedbuild fixed

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 rm -rf build
-mkdir -p build/Debug
-mkdir -p build/Release
-mkdir -p build/conan
+mkdir -p build/{Debug,Release,conan}
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan install -if build/conan .
 cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B build/Debug .
--- sharedbuild/build.sh
+++ sharedbuild/build.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 rm -rf build
-mkdir -p build/Debug
-mkdir -p build/conan
+mkdir -p build/{Debug,Release,conan}
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan install -if build/conan .
-cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B build/Debug .
+cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B build/Debug ..
+cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B build/Release ..
 cmake --build build/Debug
+cmake --build build/Release
 cp -r build/conan/lib build/Debug
+cp -r build/conan/lib build/Release
+cp nordvpncc.sh build/Debug/bin
+cp nordvpncc.sh build/Release/bin
