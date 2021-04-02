5933f459ecb95bbb7a823dbedd81e99570785223
alwin <alwin@ibba.de>
2021-04-02 11:54:43 +0200

    tar.gz archive for binary release

--- build.sh
+++ build.sh
@@ -11,3 +11,4 @@ cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G
 cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B $BUILD/Release .
 cmake --build $BUILD/Debug
 cmake --build $BUILD/Release
+tar -czvf $BUILD/Release/bin/binary.tar.gz -C $BUILD/Release/bin nordc
