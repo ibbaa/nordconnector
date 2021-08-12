b60ca1a3817cda7a9ec59985c6b857da352024e9
Alwin Ibba <alwin.ibba@web.de>
2021-08-12 21:41:28 +0200

    docker build return value

--- docker_build.sh
+++ docker_build.sh
@@ -3,4 +3,6 @@ UID=$(id -u)
 GID=$(id -g)
 docker build --no-cache --build-arg UID=$UID --build-arg GID=$GID -t ibbaa/nordc:1.0 .
 docker run --rm -v $PWD:/var/nordc -w /var/nordc ibbaa/nordc:1.0 /bin/bash -c "./build.sh dockerbuild"
+RET=$?
 docker image prune -f
+exit $RET
