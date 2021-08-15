3f5ad9d2c96f4bf6d4feaf194c1a8d086568f37c
Alwin Ibba <alwin.ibba@web.de>
2021-08-15 15:29:01 +0200

    rename build image

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 UID=$(id -u)
 GID=$(id -g)
-docker build --no-cache --build-arg UID=$UID --build-arg GID=$GID -t ibbaa/nordc:1.0 .
-docker run --rm -v $PWD:/var/nordc -w /var/nordc ibbaa/nordc:1.0 /bin/bash -c "./build.sh dockerbuild"
+docker build --no-cache --build-arg UID=$UID --build-arg GID=$GID -t ibbaa/nordc:build .
+docker run --rm -v $PWD:/var/nordc -w /var/nordc ibbaa/nordc:build /bin/bash -c "./build.sh dockerbuild"
 RET=$?
 docker image prune -f
 exit $RET
