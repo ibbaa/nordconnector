d5997b4a640782b7b73176b16a58f4c84ae8e57e
Alwin Ibba <alwin@ibba.de>
2023-03-09 16:15:07 +0100

    Conan 2.0 prepared

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ conan profile update settings.compiler.libcxx=libstdc++11 default
 conan install -if $BUILD/conan . --build=missing
 cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B $BUILD/Debug .
 cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -G "Unix Makefiles" -B $BUILD/Release .
+# cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -G "Unix Makefiles" -B $BUILD/Debug .
+# cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -G "Unix Makefiles" -B $BUILD/Release .
 cmake --build $BUILD/Debug
 cmake --build $BUILD/Release
 tar -czvf $BUILD/Release/bin/binary.tar.gz -C $BUILD/Release/bin nordc
