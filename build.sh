e6a2adaf9fc3f154ca29d2ef9a2a733d1bff432b
Alwin Ibba <alwin@ibba.de>
2023-04-30 23:18:19 +0200

    cosmetics

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ fi
 rm -rf $BUILD
 mkdir -p $BUILD/{Debug,Release}
 conan profile detect --force
-conan install . -s compiler.cppstd=gnu17 --output-folder=build --build=missing --settings=build_type=Release
-conan install . -s compiler.cppstd=gnu17 --output-folder=build --build=missing --settings=build_type=Debug
+conan install . --output-folder=build --build=missing --settings=build_type=Release --settings=compiler.cppstd=gnu17 
+conan install . --output-folder=build --build=missing --settings=build_type=Debug --settings=compiler.cppstd=gnu17 
 cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=./build/conan_toolchain.cmake -G "Unix Makefiles" -B $BUILD/Debug .
 cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=ON -DCMAKE_TOOLCHAIN_FILE=./build/conan_toolchain.cmake -G "Unix Makefiles" -B $BUILD/Release .
 cmake --build $BUILD/Debug
